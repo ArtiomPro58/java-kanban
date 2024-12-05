9c884bf211b1849fc163690ccb4ff589a92f6945
Artiom Prokopenko <artiomprokopencko@yandex.ru>
2024-12-05 15:46:44 +0300

    on the review mk7

---
+++ src/service/Node.java
@@ -0,0 +1,37 @@
+package service;
+
+import model.*;
+
+public class Node {
+    private Task task;
+    private Node prev;
+    private Node next;
+
+    public Node(Task task) {
+        this.task = task;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public Task getTask() {
+        return task;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
+}
