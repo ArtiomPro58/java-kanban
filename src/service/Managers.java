24961fda0615fe71a6fcf3e19ed22ea68175d6f6
Artiom Prokopenko <artiomprokopencko@yandex.ru>
2025-01-23 14:37:27 +0300

    edits

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -2,10 +2,12 @@ package service;
 
 public class Managers {
     public static InMemoryTaskManager getDefault() {
+
         return new InMemoryTaskManager();
     }
 
     public static InMemoryHistoryManager getDefaultHistory() {
+
         return new InMemoryHistoryManager();
     }
 }
